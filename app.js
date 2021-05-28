25a711a1178523c17caf62638657857bb3b5ed39
mmoya26 <mmoya18@icloud.com>
2021-05-28 02:00:30 -0500

    Generate tiles with JS

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 let Board = (function () {
     let gameBoard = ["","","","","","","","","",];
+
+    const setUp = () => {
+        const tilesContainer = document.querySelector(".tiles_container");
+
+        for (let i = 1; i < 10; i++) {
+            let tileElement = document.createElement("div");
+            tileElement.classList.add("tile");
+            tileElement.id = i;
+            tilesContainer.appendChild(tileElement)
+        }
+    };
+
+    return {
+        gameBoard,
+        setUp
+    }
+
 })();
+
+// let displayController = (function () {
+
+// })();
+
+
+Board.setUp();
